4c74c349c5a67781809edbe2128b3c0eba9e1874
mntky <djminatoo@gmail.com>
2019-10-07 18:23:28 +0900

    add call graph tool

--- cmd/test/testoo.go
+++ cmd/test/testoo.go
@@ -32,10 +32,6 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("testoo called")
-	},
-
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			return errors.New("requires name")
